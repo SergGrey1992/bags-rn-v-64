821a58f1b857aea2e741c78a9aec4bd9a39d1697
it-incubator-serg <sergdiag19@gmail.com>
2022-04-29 22:07:42 +0300

    adddd

--- App.tsx
+++ App.tsx
@@ -1,142 +1,292 @@
 import {StatusBar} from 'expo-status-bar';
-import {Animated, StyleSheet, Text, View, Button, LayoutChangeEvent} from 'react-native';
+import {
+    Animated,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    LayoutChangeEvent,
+    TouchableOpacity,
+    TextInput,
+    ScrollView, SafeAreaView, FlatList, ListRenderItem
+} from 'react-native';
 import {NativeBaseProvider, Box, Modal} from "native-base";
-import React, {useRef, useState} from "react";
+import React, {MutableRefObject, useRef, useState} from "react";
+import ActionSheet, {SheetManager} from "react-native-actions-sheet";
 
 
-const Test1 = ({animatedValue}:any) => {
+const Sheets = {
+    testSheet: 'test_sheet_id',
+};
 
-    const start = () => {
-        Animated.timing(animatedValue, {
-            toValue: 1,
-            useNativeDriver: false
-        }).start()
-    }
+const colors = ['#4a4e4d', '#0e9aa7', '#3da4ab', '#f6cd61', '#fe8a71'];
 
-    return <View style={{flex: 1, justifyContent:'center', alignItems:'center', backgroundColor: '#fcb0b0'}}>
-        <Button title={'step 2'} onPress={start}/>
-    </View>
-}
-const Test2 = ({animatedValue}:any) => {
-    const start = () => {
-        Animated.timing(animatedValue, {
-            toValue: 2,
-            useNativeDriver: false
-        }).start()
-    }
 
-    return <View style={{flex: 1, justifyContent:'center', alignItems:'center', backgroundColor: '#dafcb0'}}>
-        <Button title={'step 3'} onPress={start}/>
-    </View>
-}
-const Test3 = ({animatedValue}:any) => {
-    const start = () => {
-        Animated.timing(animatedValue, {
-            toValue: 0,
-            useNativeDriver: false
-        }).start()
-    }
-    return <View style={{flex: 1, justifyContent:'center', alignItems:'center', backgroundColor: '#b0fcea'}}>
-        <Button title={'step 1 and close'} onPress={start}/>
-    </View>
+const arr = new Array(40).fill(null).map((el, index) => ({id: index + 1, name: 'Test_' + index}))
+
+type MultiSelectorPropsType = {
+    arrSelectItem: Array<{ id: number; name: string }>
+    milty: boolean
+    getData: (id: number) => void
+    getLabel: (label: string) => void
+    choiceElement: Array<number>
 }
 
+const MultiSelector = (props: MultiSelectorPropsType) => {
+    const actionSheetRef = useRef<ActionSheet | null>(null);
+    const render: ListRenderItem<{ id: number; name: string }> = ({item}) => {
 
-type MeasuresType = {
-    x: number
-    y: number
-    width: number
-    height: number
+        const isChoice = props.choiceElement.find((el) => el === item.id)
+
+        return (<TouchableOpacity
+                onPress={() => {
+                    if (props.milty) {
+                        actionSheetRef.current?.snapToOffset(500)
+                        props.getData(item.id)
+                        props.getLabel(item.name)
+                        return
+                    } else {
+                        if (props.choiceElement.length === 1) {
+                            SheetManager.hide(Sheets.testSheet)
+                            return
+                        }
+                        props.getData(item.id)
+                        props.getLabel(item.name)
+                        SheetManager.hide(Sheets.testSheet)
+                    }
+                    actionSheetRef.current?.snapToOffset(500);
+                }}
+                key={`arrSelectItem.${item.name}.${item.id}`}
+                style={[styles.circle]}
+            >
+                <Text style={{backgroundColor: isChoice ? 'red' : 'white'}}>{item.name}</Text>
+            </TouchableOpacity>
+        )
+
+    }
+    return (<>
+        <ActionSheet
+        initialOffsetFromBottom={0.4}
+        onBeforeShow={data => console.log(data)}
+        id={Sheets.testSheet}
+        ref={actionSheetRef}
+        statusBarTranslucent
+        bounceOnOpen={true}
+        drawUnderStatusBar={true}
+        bounciness={4}
+        gestureEnabled={true}
+        CustomHeaderComponent={<View>
+            <Text>Header</Text>
+        </View>}
+
+        defaultOverlayOpacity={0.3}>
+        <View
+            style={{
+                paddingHorizontal: 12,
+            }}>
+            <FlatList
+                data={props.arrSelectItem}
+                renderItem={render}
+                keyExtractor={(item) => `${item.id}.${item.name}`}
+            />
+            {/*<ScrollView */}
+            {/*    contentContainerStyle={styles.container}*/}
+            {/*>*/}
+            {/*    {props.arrSelectItem.map((item, index) => {*/}
+            {/*        const isChoice = props.choiceElement.find((el) => el === item.id)*/}
+            {/*        return (<TouchableOpacity*/}
+            {/*                onPress={() => {*/}
+            {/*                    //console.log('actionSheetRef press', actionSheetRef)*/}
+            {/*                    if (props.milty) {*/}
+            {/*                        actionSheetRef.current?.snapToOffset(500)*/}
+            {/*                        props.getData(item.id)*/}
+            {/*                    } else {*/}
+            {/*                        SheetManager.hide(Sheets.testSheet)*/}
+            {/*                    }*/}
+            {/*                    actionSheetRef.current?.snapToOffset(500);*/}
+            {/*                }}*/}
+            {/*                key={`arrSelectItem.${item.name}.${item.id}`}*/}
+            {/*                style={[styles.circle]}*/}
+            {/*            >*/}
+            {/*                <Text style={{backgroundColor: isChoice ? 'red' : 'white'}}>{item.name}</Text>*/}
+            {/*            </TouchableOpacity>*/}
+            {/*        )*/}
+            {/*    })}*/}
+            {/*</ScrollView>*/}
+        </View>
+    </ActionSheet></>)
 }
 
 export default function App() {
-    const [open, setOpen] = useState(false)
-    const [measures, setMeasures] = React.useState<MeasuresType>({width: 0, height: 0, y: 0, x: 0})
-    const animatedValue = useRef(new Animated.Value(0.01)).current
-
-
-    const closed = () => {
-        Animated.timing(animatedValue, {
-            toValue: 0,
-            delay: 1000,
-            useNativeDriver: false
-        }).start()
+    const [data, setData] = useState<Array<number>>([])
+    const [label, setLabel] = useState<Array<string>>([])
+    const getData = (id: number) => {
+        const repeating = data.find((el) => el === id)
+        if (repeating) {
+            //data.filter((el) => el !== repeating)
+            setData(data.filter((el) => el !== repeating))
+        } else {
+            setData([...data, id])
+        }
     }
-
-    const start = () => {
-        Animated.timing(animatedValue, {
-            toValue: 1,
-            useNativeDriver: false
-        }).start()
-    }
-    const getLayout = (event: LayoutChangeEvent) => {
-        // m.push({
-        //     x: event.nativeEvent.layout.x,
-        //     y: event.nativeEvent.layout.y,
-        //     height: event.nativeEvent.layout.height,
-        //     width: event.nativeEvent.layout.width,
-        // })
-        //
-        // const sorting = m
-        //     .sort((a, b) => a.x - b.x)
-        //     .map((el) => el)
-        setMeasures({
-            x: event.nativeEvent.layout.x,
-            y: event.nativeEvent.layout.y,
-            height: event.nativeEvent.layout.height,
-            width: event.nativeEvent.layout.width,
-        })
-        //console.log('mm', m)
+    const getLabel = (label: string) => {
+        setLabel([label])
     }
 
     return (
-        <NativeBaseProvider>
-            <View style={{flex: 1, justifyContent: 'center'}}>
-                <Button title={'open modal'} onPress={() => setOpen(true)}/>
-                <Modal
-                    style={{paddingTop: 100, backgroundColor: 'rgba(0, 0, 0, .6)'}}
-                    justifyContent={'flex-start'}
-                    isOpen={open}
-                    onClose={() => setOpen(false)}
-                >
-                    <Modal.Content style={styles.modalContent}>
-                        <Modal.Body _scrollview={{scrollEnabled: false}} p={0}>
-                            <Animated.View
-                                onLayout={getLayout}
-                                style={[{flexDirection: 'row', transform: [{
-                                        translateX: animatedValue.interpolate({
-                                            inputRange: [0,1,2],
-                                            outputRange: [0,-(measures.width), -(measures.width * 2)]
-                                        })
-                                    }]}]}>
-                                <View style={{width: measures.width}}>
-                                    <Test1 animatedValue={animatedValue}/>
-                                </View>
-                                <View style={{width: measures.width}}>
-                                    <Test2 animatedValue={animatedValue}/>
-                                </View>
-                                <View style={{backgroundColor: 'blue', width: measures.width, height: 100}}>
-                                    <Test3 animatedValue={animatedValue}/>
-                                </View>
-                            </Animated.View>
-                        </Modal.Body>
-                    </Modal.Content>
-                </Modal>
-            </View>
-        </NativeBaseProvider>
-
-    );
+        <>
+            <SafeAreaView style={styles.safeareview}>
+                <TouchableOpacity
+                    onPress={() => {
+                        SheetManager.show(Sheets.testSheet);
+                    }}
+                    style={styles.btn}>
+                    <Text style={styles.btnTitle}>Open ActionSheet</Text>
+                </TouchableOpacity>
+                <View>
+                    <Text>{label}</Text>
+                </View>
+                <MultiSelector
+                    milty={true}
+                    arrSelectItem={arr}
+                    getData={getData}
+                    getLabel={getLabel}
+                    choiceElement={data}
+                />
+                {/*<ActionSheet*/}
+                {/*    initialOffsetFromBottom={0.4}*/}
+                {/*    onBeforeShow={data => console.log(data)}*/}
+                {/*    id={Sheets.testSheet}*/}
+                {/*    ref={actionSheetRef}*/}
+                {/*    statusBarTranslucent*/}
+                {/*    bounceOnOpen={true}*/}
+                {/*    drawUnderStatusBar={true}*/}
+                {/*    bounciness={4}*/}
+                {/*    gestureEnabled={true}*/}
+                {/*    defaultOverlayOpacity={0.3}>*/}
+                {/*    <View*/}
+                {/*        style={{*/}
+                {/*            paddingHorizontal: 12,*/}
+                {/*        }}>*/}
+                {/*        <View style={styles.container}>*/}
+                {/*            {colors.map(color => (*/}
+                {/*                <TouchableOpacity*/}
+                {/*                    onPress={() => {*/}
+                {/*                        actionSheetRef.current?.snapToOffset(500);*/}
+                {/*                    }}*/}
+                {/*                    key={color}*/}
+                {/*                    style={[*/}
+                {/*                        styles.circle,*/}
+                {/*                        {*/}
+                {/*                            backgroundColor: color,*/}
+                {/*                        },*/}
+                {/*                    ]}*/}
+                {/*                />*/}
+                {/*            ))}*/}
+                {/*        </View>*/}
+                {/*    </View>*/}
+                {/*</ActionSheet>*/}
+            </SafeAreaView>
+        </>
+    )
 }
 
+const items = [
+    100,
+    60,
+    150,
+    200,
+    170,
+    80,
+    41,
+    101,
+    61,
+    151,
+    202,
+    172,
+    82,
+    43,
+    103,
+    64,
+    155,
+    205,
+    176,
+    86,
+    46,
+    106,
+    66,
+    152,
+    203,
+    173,
+    81,
+    42,
+];
+
 const styles = StyleSheet.create({
-    modalContent: {
-        width: '90%',
-        borderRadius: 2
+    footer: {
+        height: 100,
     },
-    headerText: {
-        color: '#737067',
-        fontStyle: 'normal',
-        fontWeight: '500',
-        fontSize: 20,
-    }
-})
+    listItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    placeholder: {
+        height: 15,
+        backgroundColor: '#f0f0f0',
+        marginVertical: 15,
+        borderRadius: 5,
+    },
+    circle: {
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+    },
+    btnLeft: {
+        width: 30,
+        height: 30,
+        backgroundColor: '#f0f0f0',
+        borderRadius: 100,
+    },
+    input: {
+        width: '100%',
+        minHeight: 50,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#f0f0f0',
+        marginBottom: 15,
+        paddingHorizontal: 10,
+    },
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 15,
+    },
+    scrollview: {
+        width: '100%',
+        padding: 12,
+    },
+    btn: {
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        backgroundColor: '#fe8a71',
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        elevation: 5,
+        shadowColor: 'black',
+        shadowOffset: {width: 0.3 * 4, height: 0.5 * 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 0.7 * 4,
+    },
+    safeareview: {
+        justifyContent: 'center',
+        flex: 1,
+    },
+    btnTitle: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+});
